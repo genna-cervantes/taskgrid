19eb75716e538ad4b62f5eca6b2ecacc02b1ae19
genna-cervantes <gennacervantes9@gmail.com>
2025-05-07 12:24:28 +0800

    feat: trpc security endpoint

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -11,8 +11,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://taskan.cloud'],
+    methods: ['GET', 'POST', 'PUT']
+}));
+
 app.use(express.json());
+
+// set headers
+app.use((req, res, next) => {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  next();
+});
 
 app.use("/trpc", 
   createExpressMiddleware({
