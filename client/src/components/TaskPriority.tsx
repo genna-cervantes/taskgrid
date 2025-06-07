971e45aa43001c9e50886d495d9d7d2777c6f533
genna-cervantes <gennacervantes9@gmail.com>
2025-06-07 13:39:50 +0800

    style: replaced circle with arrow in task prio

--- client/src/components/TaskPriority.tsx
+++ client/src/components/TaskPriority.tsx
@@ -2,28 +2,25 @@ const TaskPriority = ({priority}: {priority: string}) => {
 
     if (priority === 'low'){
         return (
-          <div className="flex gap-1">
-            <span className="w-1 h-1 md:w-2 md:h-2 rounded-full bg-green-400 inline-block" />
-            {/* <span className="w-2 h-2 rounded-full bg-[#F5F5F5] inline-block" />
-            <span className="w-2 h-2 rounded-full bg-[#F5F5F5] inline-block" /> */}
+          <div className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up-icon lucide-chevron-up text-green-400 inline-block rotate-180"><path d="m18 15-6-6-6 6"/></svg>
           </div>
         )
     }
     if (priority === 'medium'){
         return (
-          <div className="flex gap-1">
-            <span className="w-1 h-1 md:w-2 md:h-2 rounded-full bg-orange-400 inline-block" />
-            <span className="w-1 h-1 md:w-2 md:h-2 rounded-full bg-orange-400 inline-block" />
-            {/* <span className="w-2 h-2 rounded-full bg-[#F5F5F5] inline-block" /> */}
+          <div className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up-icon lucide-chevron-up text-orange-400 inline-block"><path d="m18 15-6-6-6 6"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up-icon lucide-chevron-up text-orange-400 inline-block"><path d="m18 15-6-6-6 6"/></svg>
           </div>
         )
     }
     if (priority === 'high'){
         return (
-          <div className="flex gap-1">
-            <span className="w-1 h-1 md:w-2 md:h-2 rounded-full bg-red-400 inline-block" />
-            <span className="w-1 h-1 md:w-2 md:h-2 rounded-full bg-red-400 inline-block" />
-            <span className="w-1 h-1 md:w-2 md:h-2 rounded-full bg-red-400 inline-block" />
+          <div className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up-icon lucide-chevron-up text-red-400 inline-block"><path d="m18 15-6-6-6 6"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up-icon lucide-chevron-up text-red-400 inline-block"><path d="m18 15-6-6-6 6"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-up-icon lucide-chevron-up text-red-400 inline-block"><path d="m18 15-6-6-6 6"/></svg>
           </div>
         )
 
