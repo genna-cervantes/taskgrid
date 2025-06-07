acbbcc5ee1dfdc6e320ae1de769f0bd72a113aaa
genna-cervantes <gennacervantes9@gmail.com>
2025-06-07 14:02:29 +0800

    style: task block details and column details

--- client/src/components/AddTask.tsx
+++ client/src/components/AddTask.tsx
@@ -3,18 +3,26 @@ import { cn } from "../utils/utils";
 import { ColumnKey } from "../../../server/src/shared/types";
 import AddTaskForm from "./AddTaskForm";
 
-const AddTask = ({username, projectId, col, className = ""}: {username: string|undefined, projectId: string, col: ColumnKey, className?: string}) => {
+const AddTask = ({type, username, projectId, col, className = ""}: {type: string, username: string|undefined, projectId: string, col: ColumnKey, className?: string}) => {
   const [addModal, setAddModal] = useState(false);
 
   if (addModal){
     return <AddTaskForm username={username} projectId={projectId} col={col} setAddModal={setAddModal} />
   }
 
-  return (
-    <button onClick={() => setAddModal(true)} className={cn("border-2 border-[#464646] text-xs text-[#464646] text-center rounded-md w-full py-2 font-bold cursor-pointer hover:text-white/70 hover:border-white/70", className)}>
-      Add Task
-    </button>
-  );
+  if (type === 'block'){
+    return (
+      <button onClick={() => setAddModal(true)} className={cn("border-2 border-[#464646] text-xs text-[#464646] text-center rounded-md w-full py-2 font-bold cursor-pointer hover:text-white/70 hover:border-white/70", className)}>
+        Add Task
+      </button>
+    );
+  }else{
+    return (
+      <button onClick={() => setAddModal(true)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-plus-icon lucide-plus text-[#464646] hover:text-white/70"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+      </button>
+    )
+  }
 };
 
 export default AddTask;
--- client/src/components/TaskBlock.tsx
+++ client/src/components/TaskBlock.tsx
@@ -28,12 +28,12 @@ const TaskBlock = ({
         draggable
         onDragStart={() => handleDragStart(col, task)}
         onClick={() => setTaskDetailsModal(true)}
-        className="px-4 py-3 mb-2 bg-[#464646] rounded-md cursor-move border-2 border-gray-600/30 shadow-sm"
+        className="px-3 py-3 mb-2 bg-[#464646] rounded-md cursor-move border-2 border-gray-600/30 shadow-sm"
       >
-        <h1 className="text-sm truncate">{task.title}</h1>
-        <div className="mt-4">
+        <h1 className="text-sm line-clamp-2" title={task.title}>{task.title}</h1>
+        <div className="mt-2">
           <TaskPriority priority={task.priority} />
-          <div className="flex justify-between text-xs pt-1">
+          <div className="flex justify-between text-xs">
             <p className="font-semibold">[{task.projectTaskId}]</p>
             <p>{task.assignedTo} {task.assignedTo === username ? '(You)' : ''}</p>
           </div>
