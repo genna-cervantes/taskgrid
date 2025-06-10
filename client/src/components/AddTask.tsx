4c32d768d28a8ffbbd9906f8ac6a214e89b8799c
genna-cervantes <gennacervantes9@gmail.com>
2025-06-10 20:48:46 +0800

    feat: add titles/tooltips to icon buttons

--- client/src/components/AddTask.tsx
+++ client/src/components/AddTask.tsx
@@ -18,7 +18,7 @@ const AddTask = ({type, username, projectId, col, className = ""}: {type: string
     );
   }else{
     return (
-      <button onClick={() => setAddModal(true)}>
+      <button title="Add Task" onClick={() => setAddModal(true)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus-icon lucide-plus text-[#464646] hover:text-white/70"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
       </button>
     )
--- client/src/components/ClearTask.tsx
+++ client/src/components/ClearTask.tsx
@@ -30,7 +30,7 @@ const ClearTask = ({
         projectId={projectId}
         tasks={tasks}
       />
-      <button onClick={handleClickClearTask}>
+      <button onClick={handleClickClearTask} title="Archive Column">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="18"
